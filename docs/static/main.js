3d61161727c98096cf9b3e4218e77693f67da460
Arunmozhi <arun@arunmozhi.in>
2022-01-09 09:26:18 +0530

    a lot of rework around text input and verification

--- docs/static/main.js
+++ docs/static/main.js
@@ -1,17 +1,39 @@
 // container for storing globals
 var store = window.localStorage;
 var APP = { wordLength: 5, history: [] };
+var diacritics = {
+  "\u0B82": true,
+  "\u0BBE": true,
+  "\u0BBF": true,
+  "\u0BC0": true,
+  "\u0BC1": true,
+  "\u0BC2": true,
+  "\u0BC6": true,
+  "\u0BC7": true,
+  "\u0BC8": true,
+  "\u0BCA": true,
+  "\u0BCB": true,
+  "\u0BCC": true,
+  "\u0BCD": true,
+  "\u0BD7": true,
+};
 
 /**
  * Mottie Keyboard related stuff
  *
  * */
-$("#keyboard").keyboard({
-  usePreview: false,
-  layout: "tamil-tamil99-mod",
-  autoAccept: true,
-  initialFocus: false,
-});
+function initKeyboard() {
+  $("#keyboard").val("");
+  $("#keyboard").keyboard({
+    usePreview: false,
+    layout: "tamil-tamil99-mod",
+    autoAccept: true,
+    alwaysOpen: true,
+    change: function () {
+      renderWorkarea();
+    },
+  });
+}
 
 /**
  * Convert a string into an array of strings representing Tamil letters
@@ -20,23 +42,6 @@ $("#keyboard").keyboard({
  * @returns an Array of strings that is equivalent to letters in Tamil
  */
 function convertToLetters(word) {
-  var diacritics = {
-    "\u0B82": true,
-    "\u0BBE": true,
-    "\u0BBF": true,
-    "\u0BC0": true,
-    "\u0BC1": true,
-    "\u0BC2": true,
-    "\u0BC6": true,
-    "\u0BC7": true,
-    "\u0BC8": true,
-    "\u0BCA": true,
-    "\u0BCB": true,
-    "\u0BCC": true,
-    "\u0BCD": true,
-    "\u0BD7": true,
-  };
-
   var targetList = [];
   for (var i = 0; i != word.length; i++) {
     var ch = word[i];
@@ -47,14 +52,24 @@ function convertToLetters(word) {
   return targetList;
 }
 
+function renderWorkarea() {
+  var word = $("#keyboard").val();
+  var letters = convertToLetters(word);
+  var workarea = $("#workarea");
+  workarea.empty();
+  for (var i = 0; i < APP.wordLength; i++) {
+    var _class = "letter-box" + (letters[i] ? " b-dark" : "");
+    workarea.append(`<div class="${_class}">${letters[i] || ""}</div>`);
+  }
+}
+
 /**
  * Uses the response from the server to populate the UI
  *
  * @param {array} letters Array of string - equivalent to Tamil Letters
  * @param {XMLHttpRequest} http HTTP request object with responseText
  */
-function updateResults(letters, http) {
-  var status = JSON.parse(http.responseText);
+function updateResults(letters, status) {
   var box = {
     LETTER_NOT_FOUND: "⚫",
     LETTER_ELSEWHERE: "🟡",
@@ -84,6 +99,8 @@ function updateResults(letters, http) {
   store.setItem("historyBlocks", JSON.stringify(APP.history));
 
   $("#historyDiv").append(historyItem);
+  $("#keyboard").val("");
+  renderWorkarea();
 }
 
 function showError(message) {
@@ -99,31 +116,33 @@ function showSuccess() {
  * Processes the value in the Input Box
  */
 function process() {
-  var str = $("#keyboard").val().trim();
-  var targetList = convertToLetters(str);
-
-  const http = new XMLHttpRequest();
-  http.open("POST", "https://tamilwordle-maleycpqdq-el.a.run.app/verify-word");
-  http.setRequestHeader("Content-Type", "application/json");
-  http.send(JSON.stringify(targetList));
-
-  http.onreadystatechange = (e) => {
-    if (http.readyState === XMLHttpRequest.DONE) {
-      switch (http.status) {
+  var word = $("#keyboard").val();
+  var letters = convertToLetters(word);
+  $.ajax({
+    method: "POST",
+    url: "https://tamilwordle-maleycpqdq-el.a.run.app/verify-word",
+    data: JSON.stringify(letters),
+    dataType: "text",
+    contentType: "application/json",
+    success: function (data, status, xhr) {
+      switch (xhr.status) {
         case 202:
           APP.history.push("🟩".repeat(APP.wordLength));
           store.setItem("historyBlocks", JSON.stringify(APP.history));
           showSuccess();
           return;
         case 200:
-          updateResults(targetList, http);
+          updateResults(letters, JSON.parse(data));
           return;
         default:
           showError(http.responseText || "Network Error");
           return;
       }
-    }
-  };
+    },
+  }).fail(function () {
+    showError("Failed to check your word.");
+  });
+  $("#keybaord").val("");
 }
 
 /**
@@ -131,38 +150,35 @@ function process() {
  */
 function init() {
   $("#lengthLabel").text(APP.wordLength);
+
   // See if the game has already been played today
-  // If it is already theres, just show the success dialog
-  var lastDate = parseInt(store.getItem("date"));
+  // If it is already there, just show the success dialog
+  var lastDate = new Date().toDateString();
   if (lastDate === new Date().toDateString()) {
     APP.history = JSON.parse(store.getItem("historyBlocks"));
     showSuccess();
     return;
   }
-
   store.setItem("date", new Date().toDateString());
+  initKeyboard();
 
-  const http = new XMLHttpRequest();
-  http.open(
-    "GET",
-    "https://tamilwordle-maleycpqdq-el.a.run.app/get-current-word-len"
-  );
-  http.onreadystatechange = (e) => {
-    if (http.readyState === XMLHttpRequest.DONE) {
-      var status = http.status;
-
-      if (status !== 200) {
-        console.log("some error happened", http.status);
-        showError("Error loading the game!");
-        return;
-      }
-
-      var jsonResponse = JSON.parse(http.responseText);
-      APP.wordLength = jsonResponse.length;
+  $.ajax({
+    url: "https://tamilwordle-maleycpqdq-el.a.run.app/get-current-word-len",
+    success: function (data) {
+      APP.wordLength = JSON.parse(data).Length;
       $("#lengthLabel").text(APP.wordLength);
-    }
-  };
-  http.send();
+      var workarea = $("#workarea");
+      for (var i = 0; i < APP.wordLength; i++) {
+        workarea.append(`<div class="letter-box"></div>`);
+      }
+      $("#verify-button").css({
+        width: (64 + 4) * APP.wordLength + 0.75 * (APP.wordLength - 1) + "px",
+      });
+    },
+  }).fail(function () {
+    console.log("some error happened", http.status);
+    showError("Error loading the game!");
+  });
 }
 
 /**
