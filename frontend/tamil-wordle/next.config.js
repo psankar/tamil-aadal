843de07d638816dbb4560c91aa18347b554317b0
Sureshkumar T <tsureshkumar@gmail.com>
2022-01-19 11:22:06 +0530

    update backend server

--- frontend/tamil-wordle/next.config.js
+++ frontend/tamil-wordle/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-      backend_server: 'https://tamilwordle-maleycpqdq-el.a.run.app',
-      backend_server1: 'https://uyirmei-maleycpqdq-el.a.run.app',
+      backend_server1: 'https://tamilwordle-maleycpqdq-el.a.run.app',
+      backend_server: 'https://uyirmei-maleycpqdq-el.a.run.app',
       end_point1: 'verify-word',
       end_point: 'verify-word-with-uyirmei',
   },
