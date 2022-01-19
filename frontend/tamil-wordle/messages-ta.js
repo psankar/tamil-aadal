b316e09b8f2cf732be61c20f14336c7849e8dd72
Sureshkumar T <tsureshkumar@gmail.com>
2022-01-19 11:19:56 +0530

    update title word

--- frontend/tamil-wordle/messages-ta.js
+++ frontend/tamil-wordle/messages-ta.js
@@ -1,8 +1,9 @@
 export const IntlMsg = {
-    game_name: "தமிழ் சொல் விளையாட்டு",
+    game_name: "தமிழ் சொல்விளையாட்டு",
     btn_try: "சரி பார்க்க",
     btn_game_over: "கெலிச்சாச்சு! பகிரவும்!",
     btn_final_message: "சரியான சொல்லைக் கண்டுபிடித்துவிட்டீர்கள்!!! If you are interested, copy and paste the emoji table to social media!",
     msg_invalid_length: "*சரியான எண்ணிக்கையிலான எழுத்துக்களை இடவும்",
     msg_duplicate: "*ஏற்கனவே முயன்றது!",
+    try_uyirmei: "Try உயிர்மெய் version"
 }
