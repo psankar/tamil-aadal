cfa34fa1379a034410bc05777b462455b335797f
vinay-kumaar <98677775+vinay-kumaar@users.noreply.github.com>
2022-02-09 21:40:22 +0530

    Word for Feb 10

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "வினையூக்கி"
+	return "கிளிஞ்சல்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
