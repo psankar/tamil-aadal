d6718440fd183b694bb2cf87a09e2120fb70aab7
Elavasam <99493083+Elavasam@users.noreply.github.com>
2022-02-13 11:20:36 -0600

    updated word for Feb 14th

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "வானம்பாடி"
+	return "கண்ணாட்டி"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
