f3d9d4237b092abcd5d153d05745552c3244d102
Elavasam <99493083+Elavasam@users.noreply.github.com>
2022-02-16 09:43:44 -0600

    Word for Feb 17th

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "படகோட்டி"
+	return "இங்ஙனம்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
