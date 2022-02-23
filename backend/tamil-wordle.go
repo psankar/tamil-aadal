4044fc30c3eee373dc8afeca473ae79886d05bfe
Elavasam <99493083+Elavasam@users.noreply.github.com>
2022-02-23 10:31:45 -0600

    Updated for Feb 24th

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "திறப்பாடு"
+	return "ஒப்புரவு"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
