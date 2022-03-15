d001c3222fd3e5014852c7ace738045dd979f205
tcsprasan <100919081+tcsprasan@users.noreply.github.com>
2022-03-15 18:35:54 +0530

    Changed the word for 16 March

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "நெகிழ்ந்து"
+	return "வேய்ங்குழல்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
