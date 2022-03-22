5738b2e256a89eb2e743669db23477778a41487b
tcsprasan <100919081+tcsprasan@users.noreply.github.com>
2022-03-22 15:29:51 +0530

    Word for 23rd March updated - Prasanna

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "பூஞ்சிட்டு"
+	return "மனக்கணக்கு"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
