b83af67aa27ceca757f863bd1a93c5c648019b03
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-03-26 08:36:02 +0530

    Word for March 26 - psankar

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "இளவேனில்"
+	return "இராகவன்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
