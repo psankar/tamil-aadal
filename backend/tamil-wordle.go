3beb3e7d937774e5851fa3880d8adba081415fd6
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-03-30 23:59:40 +0530

    Word for March 31 Ragavan

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "தாழ்வாரம்"
+	return "தூக்குவாளி"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
