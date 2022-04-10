d094d37cbd2d263646c14b93506f039da85de8e3
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-04-10 08:44:55 +0530

    Word for Apr 10 - Sankar

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "பட்டாங்கு"
+	return "தமிழணங்கு"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
