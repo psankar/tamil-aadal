bce67b50c1c2c621e712103fee2cebdfca713ab4
gragavan <102863284+gragavan@users.noreply.github.com>
2022-04-11 17:03:53 +0530

    Word for April 12 2022 - Ragavan

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "உறக்கம்"
+	return "பட்டாங்கு"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
