3db7c89427ea3a3f63a17aedade8f305e8c9317e
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-04-27 08:42:47 +0530

    word for april 27 kanapraba

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "பின்னேரம்"
+	return "கமக்காரன்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
