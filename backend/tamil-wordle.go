e1c08bc32711a01e1deb268c6e6f2c3847604b6e
veeruice <31613484+veeruice@users.noreply.github.com>
2022-05-25 20:46:51 +0530

    Update tamil-wordle.go
    updated word for May 26

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "ஆகுபெயர்"
+	return "உவமையணி"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
