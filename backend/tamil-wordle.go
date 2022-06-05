f82082d76f73a6bd334bc8c281274a083073ab10
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-06-05 08:51:53 +0530

    Word for June 5 - Veeba6

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "வெஞ்சனம்"
+	return "நுகத்தடி"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
