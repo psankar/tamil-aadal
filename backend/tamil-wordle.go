881d5d2dbb89854d1d4fd0e3e968a34139e87223
Nadika N <85942764+Nadika-Nadja@users.noreply.github.com>
2022-06-26 22:41:28 +0530

    Updated word for 27 Jun 2022 Nadika

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "இடைச்சியர்"
+	return "புணர்தல்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
