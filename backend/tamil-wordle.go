9c15b8b8b3272dd6cea3d72fbc3cfe533d095c49
Balaganeshks <108991372+Balaganeshks@users.noreply.github.com>
2022-07-17 18:45:24 -0400

    Update tamil-wordle.go
    Updated for 07/18 - Piliral

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "அண்ணாங்கால்"
+	return "ஆசாரம்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
