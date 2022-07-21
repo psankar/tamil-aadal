37d747f654887b5721df8cac067003150702a8df
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-07-21 06:54:02 +0530

    Word for 21-Jul-2022 Piliral

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "ஒருபாடு"
+	return "கோசாப்பழம்"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
