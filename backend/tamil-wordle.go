0fbc8cf7f919b12deae5a67a477d2e0856ea33b2
Sankar சங்கர் <sankar.curiosity@gmail.com>
2022-07-25 08:54:38 +0530

    Word for 25-JUL-2022 Piliral

--- backend/tamil-wordle.go
+++ backend/tamil-wordle.go
@@ -63,7 +63,7 @@ type CurrentWordLenResponse struct {
 }
 
 func getWordForToday() string {
-	return "சொள்ளை"
+	return "அப்பச்சி"
 }
 
 func getCurrentWordLenHandler(w http.ResponseWriter, r *http.Request) {
